f2391f597208e1d83f86237acc06d3d015b0e1ef
KowsalyaBaskar <kowsalyab2611@gmail.com>
2024-09-13 02:35:53 +0530

    kowsi code -2

--- src/test/java/org/base/BaseClass.java
+++ src/test/java/org/base/BaseClass.java
@@ -1,7 +1,20 @@
 package org.base;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.DateUtil;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -63,6 +76,7 @@ public class BaseClass {
 		return text;
 	}
 	
+	
 	public static String getAttribute(WebElement e) {
 		String attribute = e.getAttribute("value");
 		return attribute;
@@ -111,11 +125,54 @@ public class BaseClass {
 		driver.get(a);
 		return driver;
 	}
+	public static String readExcel(String filename, String sheet, int row, int cell) throws IOException {
+		File f = new File("C:\\Users\\Kowsalya Baskar\\eclipse-workspace\\Maven\\src\\test\\resources\\"+filename+".xlsx");
+		FileInputStream st = new FileInputStream(f);
+		Workbook w = new XSSFWorkbook(st);
+		Sheet s = w.getSheet(sheet);
+		Row r = s.getRow(row);
+		Cell c = r.getCell(cell);
 	
-	
-	
-	
+		int type = c.getCellType();
+		String value = null;
+		if(type==1)
+		{
+			value = c.getStringCellValue();
+		}
+		else
+		{
+			if(DateUtil.isCellDateFormatted(c))
+			{
+				Date dateCellValue = c.getDateCellValue();
+				SimpleDateFormat sd = new SimpleDateFormat("dd-MMM-yyyy");
+				value = sd.format(dateCellValue);
+			}
+			else
+			{
+				double numericCellValue = c.getNumericCellValue();
+				long num=(long)numericCellValue;
+				value = String.valueOf(num);
+			}
+		}
+		return value;
 	}
-
 	
+	public static int extractNumber(String text) {
+	    
+	    Pattern pattern = Pattern.compile("of (\\d+)");
+	    Matcher matcher = pattern.matcher(text);
 
+	    if (matcher.find()) {
+	        return Integer.parseInt(matcher.group(1));
+	    }
+		return 0;  
+	}
+	public static int extractNumberAfterHyphen(String text) {
+	    Pattern pattern = Pattern.compile("– (\\d+)");
+	    Matcher matcher = pattern.matcher(text);
+	    if (matcher.find()) {
+	        return Integer.parseInt(matcher.group(1));
+	    }
+	    return 0; 
+	}
+	}
